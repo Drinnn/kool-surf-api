45e20ecca324e59e2fab759d680dfc794f2d49af
Drinnn <pedro_lontro@hotmail.com>
2022-02-04 15:37:33 -0300

    feat: add forecast controller with green signal response

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "./util/module-alias";
 import { Server } from "@overnightjs/core";
 import bodyParser from "body-parser";
+import { ForecastController } from "./controllers/forecast";
 
 export class SetupServer extends Server {
   constructor(private port = 3333) {
@@ -9,9 +10,16 @@ export class SetupServer extends Server {
 
   public init(): void {
     this.setupExpress();
+    this.setupControllers();
   }
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
+  }
+
+  private setupControllers(): void {
+    const forecastController = new ForecastController();
+
+    this.addControllers([forecastController]);
   }
 }
